d90f9b5cd6447f2ccd152295065bf0a5051439c8
taitamfc <taitam8590@gmail.com>
2023-06-15 13:50:15 +0700

    Add Readme

---
+++ 001/turtle-basic.py
@@ -0,0 +1,15 @@
+# Bước 1: Nhúng thư viện vào
+import turtle
+# Bước 2: tạo đối tượng
+turtle.Turtle()
+# Bước 3: Các lệnh điều khiển
+
+# Di chuyển về phía trước
+turtle.forward(100)
+
+# Rẻ phải
+turtle.right(100)
+
+
+#Kết thúc
+turtle.done()
